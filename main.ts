c956916d75dbcc6dff542b38724f3d036c14ba98
DaxtonLovell <112964339+DaxtonLovell@users.noreply.github.com>
2024-10-23 11:59:51 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+pins.digitalWritePin(DigitalPin.P0, 0)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        pins.digitalWritePin(DigitalPin.P9, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P9, 0)
+    }
+})
